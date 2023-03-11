8b999f9199daef746824edecb167d6227e3b79e2
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-11 16:36:01 -0300

    :sparkles: create: gesture - dont (gesture.js)

--- src/gesture.js
+++ src/gesture.js
@@ -9,6 +9,7 @@ const {
 const rockGesture = new GestureDescription('rock')  // ✊️
 const paperGesture = new GestureDescription('paper')  // 🖐
 const scissorsGesture = new GestureDescription('scissors') // ✌️
+const dontGesture = new GestureDescription('dont') //🙅
 
 
 rockGesture.addCurl(Finger.Thumb, FingerCurl.HalfCurl, 1.0)
@@ -33,9 +34,25 @@ scissorsGesture.addCurl(Finger.Pinky, FingerCurl.FullCurl, 1.0);
 scissorsGesture.addCurl(Finger.Pinky, FingerCurl.HalfCurl, 0.9);
 
 
+for (const finger of Finger.all) {
+
+  dontGesture.addCurl(finger, FingerCurl.NoCurl, 1.0)
+  dontGesture.addCurl(finger, FingerCurl.HalfCurl, 0.8)
+  
+  dontGesture.addDirection(finger, FingerDirection.DiagonalUpRight, 1.0)
+  dontGesture.addDirection(finger, FingerDirection.DiagonalUpLeft, 1.0)
+
+  dontGesture.addDirection(finger, FingerDirection.HorizontalRight, 1.0)
+  dontGesture.addDirection(finger, FingerDirection.HorizontalLeft, 1.0)
+
+}
+
+
 const gestures = [ 
   rockGesture,
-  paperGesture
+  paperGesture,
+  scissorsGesture,
+  dontGesture,
 ]
 
 export {gestures}
