794e592e76d1e6d50529b632c2718e4178236159
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-11 16:38:05 -0300

    :pencil2: fix: typo - finer -> finger (gesture.js)

--- src/gesture.js
+++ src/gesture.js
@@ -15,7 +15,7 @@ const dontGesture = new GestureDescription('dont') //🙅
 rockGesture.addCurl(Finger.Thumb, FingerCurl.HalfCurl, 1.0)
 rockGesture.addCurl(Finger.Thumb, FingerCurl.NoCurl, 0.5)
 
-for(let finer of [Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
+for(let finger of [Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
   rockGesture.addCurl(finger, FingerCurl.FullCurl, 1.0)
   rockGesture.addCurl(finger, FingerCurl.HalfCurl, 0.9)
 }
