259bfeacb3ca333df688d12655107f70ca63af0b
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-11 15:42:39 -0300

    :gear: create: config - finger colors (index.js)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 
 const config = { video: {width: 640, height: 480, fps: 30} }
 
-
+const landmarkcolor = {
+  thumb: 'black',
+  index: 'blue',
+  middle: 'red',
+  ring: 'green',
+  pinky: 'yellow',
+  wirst: 'white'
+}
